aaf2cb8a8ada2a3136209f6b4eea353700925bf7
Thariq Audah <thariq.audah@gmail.com>
2022-01-24 16:34:55 +0700

    Create card and build layouts

---
+++ src/App.vue
@@ -0,0 +1,160 @@
+<template>
+	<div class="container">
+		<ul class="user-list">
+			<li class="user-item" v-for="user in users" :key="user.id">
+				<Card :user="user" />
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+
+export default {
+	name: 'App',
+	components: { Card },
+	setup() {
+		const users = ref([
+			{
+				id: 1,
+				name: 'Justin Case',
+				username: '@justin_case',
+				bio: "Hi my developer fellas, I'm Justin and I am a frontend developer. Nice to meet you!",
+				avatar: 'justin_case_avatar.jpg',
+				cover: 'justin_case_cover.jpg',
+				followers: 1300,
+				role: 'Programmer',
+				isFollowed: false,
+			},
+			{
+				id: 2,
+				name: 'Bagus Lazuardi',
+				username: '@baguslazuardi',
+				bio: "Hello friends. I'm teaching about web development and web design. Let's learn together",
+				avatar: 'bagus_lazuardi_avatar.jpg',
+				cover: 'bagus_lazuardi_cover.jpg',
+				followers: 20000,
+				role: 'Teacher',
+				isFollowed: true,
+			},
+			{
+				id: 3,
+				name: 'Will Barrow',
+				username: '@will_barrow',
+				bio: "Hi, I'm Will Barrow and I create content about programming and design on Youtube.",
+				avatar: 'will_barrow_avatar.jpg',
+				cover: 'will_barrow_cover.jpg',
+				followers: 120000,
+				role: 'Content Creator',
+				isFollowed: false,
+			},
+		]);
+
+		return {
+			users,
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	font-family: 'Montserrat', sans-serif;
+	font-size: 16px;
+	color: #444;
+}
+
+#app {
+	min-height: 100vh;
+}
+
+.container {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 24px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
+}
+
+.user-list {
+	list-style: none;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	gap: 24px;
+
+	@media (max-width: 750px) {
+		.card {
+			max-width: 75%;
+			margin: 0 auto;
+		}
+	}
+
+	@media (max-width: 480px) {
+		.card {
+			max-width: 100%;
+			margin: 0 auto;
+
+			.card-body {
+				padding: 0 16px 16px;
+			}
+		}
+	}
+
+	@media (max-width: 350px) {
+		.card {
+			.card-body {
+				.card-title {
+					.name {
+						font-size: 16px;
+					}
+					.username {
+						font-size: 14px;
+					}
+				}
+
+				.card-bio {
+					font-size: 14px;
+				}
+
+				.card-details {
+					font-size: 12px;
+				}
+			}
+		}
+	}
+}
+
+.btn {
+	display: inline-block;
+	background-color: #eee;
+	color: #444;
+	border: none;
+	padding: 8px 16px;
+	font-size: 16px;
+	font-weight: 600;
+	border-radius: 100px;
+	box-shadow: 0px 2px 5px rgba(60, 210, 120, 0.25);
+	cursor: pointer;
+	transition: all 0.2s ease-in;
+
+	&-primary {
+		background-color: #3cd278;
+		color: #fff;
+
+		&:hover {
+			background-color: darken($color: #3cd278, $amount: 10);
+		}
+	}
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,207 @@
+<template>
+	<div class="card">
+		<div
+			role="header"
+			class="card-header"
+			:style="{
+				backgroundImage: 'url(' + require('../assets/img/' + user.cover) + ')',
+			}"
+		></div>
+
+		<div class="card-body">
+			<div class="card-img">
+				<img
+					:src="require(`../assets/img/${user.avatar}`)"
+					alt="User image"
+					class="avatar"
+				/>
+				<div class="is-followed btn btn-primary">
+					<img
+						src="../assets/icons/user-plus.svg"
+						alt="User plus icon"
+						class="plus"
+					/>
+					<span>Follow</span>
+				</div>
+			</div>
+
+			<div class="card-title">
+				<p class="name">{{ user.name }}</p>
+				<p class="username">{{ user.username }}</p>
+			</div>
+			<p class="card-bio">{{ user.bio }}</p>
+
+			<div class="card-details">
+				<div class="followers">
+					<span>{{ user.followers / 1000 }}k</span>
+					<span>Followers</span>
+				</div>
+				<span class="dot" role="image"></span>
+				<div class="role">{{ user.role }}</div>
+			</div>
+
+			<div class="card-socials">
+				<a href="#" class="social-link">
+					<img
+						src="../assets/icons/facebook.svg"
+						alt="Social icon"
+						class="social-icon"
+					/>
+				</a>
+				<a href="#" class="social-link">
+					<img
+						src="../assets/icons/twitter.svg"
+						alt="Social icon"
+						class="social-icon"
+					/>
+				</a>
+				<a href="#" class="social-link">
+					<img
+						src="../assets/icons/instagram.svg"
+						alt="Social icon"
+						class="social-icon"
+					/>
+				</a>
+				<a href="#" class="social-link">
+					<img
+						src="../assets/icons/youtube.svg"
+						alt="Social icon"
+						class="social-icon"
+					/>
+				</a>
+				<a href="#" class="social-link">
+					<img
+						src="../assets/icons/link.svg"
+						alt="Social icon"
+						class="social-icon"
+					/>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UserCard',
+	props: ['user'],
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+	max-width: 360px;
+	min-width: 220px;
+	background: #fff;
+	border: 1px solid #e7e7e7;
+	border-radius: 8px;
+	overflow: hidden;
+
+	.card-header {
+		height: 150px;
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+	}
+
+	.card-body {
+		padding: 0 24px 24px;
+
+		.card-img {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			transform: translateY(-50%);
+			margin-bottom: -24px;
+
+			.avatar {
+				width: 84px;
+				height: 84px;
+				border-radius: 50%;
+				box-shadow: 0 0 0 5px #fff;
+			}
+
+			.is-followed {
+				display: flex;
+				align-items: center;
+				gap: 8px;
+			}
+		}
+
+		.card-title {
+			display: flex;
+			align-items: center;
+			gap: 4px;
+			margin-bottom: 16px;
+
+			.name {
+				font-size: 18px;
+				font-weight: 600;
+			}
+
+			.username {
+				font-size: 16px;
+				color: #949494;
+			}
+		}
+
+		.card-bio {
+			font-size: 16px;
+			line-height: 1.5;
+		}
+
+		.card-details {
+			font-size: 14px;
+			margin-top: 24px;
+			display: flex;
+			align-items: center;
+			gap: 16px;
+
+			.followers {
+				span:first-child {
+					margin-right: 4px;
+					font-weight: 700;
+				}
+
+				span:last-child {
+					font-weight: 600;
+				}
+			}
+
+			.dot {
+				width: 6px;
+				height: 6px;
+				background-color: #e7e7e7;
+				border-radius: 50%;
+			}
+
+			.role {
+				font-weight: 600;
+			}
+		}
+
+		.card-socials {
+			margin-top: 16px;
+			display: flex;
+			gap: 12px;
+
+			.social-link {
+				display: inline-block;
+				width: 32px;
+				height: 32px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border-radius: 50%;
+				background-color: #e7e7e7;
+				cursor: pointer;
+				transition: all 0.2s ease-in;
+
+				&:hover {
+					background-color: #c7c7c7;
+				}
+			}
+		}
+	}
+}
+</style>
